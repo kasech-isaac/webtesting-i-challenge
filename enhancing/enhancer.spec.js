e98eaadfbb637ad671286b8d379eabd70160520e
Kasech Isaac <kasechisaac@Kasechs-MacBook-Air.local>
2020-11-04 22:14:20 -0800

    working on repair

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,13 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describ("enhancer unit test",()=>{
+
+    it ("repairs", ()=>{
+expect(enhancer.repair({
+    name:"kasech",
+    enhancement:7,
+    durability:60,
+}))
+    })
+})
